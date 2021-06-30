dfbdd750d6d3f58f21ff282ab510b96cd726e095
Erchegyi David <edavid@3-F-5.42heilbronn.de>
2021-06-30 20:40:26 +0200

    cur prog

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: edavid <edavid@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/25 20:47:25 by edavid            #+#    #+#              #
-#    Updated: 2021/06/29 14:10:25 by edavid           ###   ########.fr        #
+#    Updated: 2021/06/30 19:42:38 by edavid           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -24,10 +24,14 @@ $(NAME) : $(objects)
 
 ft_printf.o : $(libftPath) ft_printf.h
 
-driver.o : ft_printf.h
+driver.o : ft_printf.h driver.c
+	gcc -c -g driver.c
+ft_printf.o : ft_printf.h ft_printf.c
+	gcc -c -g ft_printf.c
 
-.PHONY: all clean fclean re bonus 
-all :
+
+.PHONY: all clean fclean re bonus debugger
+all : $(NAME)
 re :
 	make clean
 	make all
@@ -40,3 +44,11 @@ test : $(objects) driver.o
 	$(CC) -o test.out $^ -Llibft -lft
 clean :
 	rm -f *.o libft/*.o
+fclean : clean
+	rm -f $(NAME)
+debugger : $(objects) driver.o
+	cd libft && $(MAKE) debugger
+	cp $(libftPath:.h=.a) .
+	mv libft.a $(NAME)
+	ar -rs $(NAME) $(objects) driver.o
+	$(CC) -g -o test.out $^ -Llibft -lft
--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: edavid <edavid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/26 14:20:43 by edavid            #+#    #+#             */
-/*   Updated: 2021/06/30 17:16:19 by edavid           ###   ########.fr       */
+/*   Updated: 2021/06/30 20:34:19 by edavid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -317,28 +317,36 @@ static int	print_conversion_p(void *arg_pointer, int *flags)
 {
 	int addr_len;
 	int	printed_spaces;
+	int	null_precision_pointer;
 
+	null_precision_pointer = 0;
+	if (!arg_pointer && flags[3] == -2)
+		null_precision_pointer = 1;
 	printed_spaces = 0;
 	addr_len = digits_in_hexa((unsigned long)arg_pointer);
 	if (flags[0])
 	{
 		write(1, "0x", 2);
-		print_ultoh((unsigned long)arg_pointer, 's');
-		while ((flags[2])-- - (addr_len + 2) > 0)
-		{
-			ft_putchar_fd(' ', 1);
-			printed_spaces++;
-		}
+		if (!null_precision_pointer)
+			print_ultoh((unsigned long)arg_pointer, 's');
+		if (flags[2] > addr_len + 2 - null_precision_pointer)
+			while (flags[2]-- > addr_len + 2 - null_precision_pointer)
+			{
+				ft_putchar_fd(' ', 1);
+				printed_spaces++;
+			}
 	}
 	else
 	{
-		while (flags[2]-- > addr_len + 2)
-		{
-			ft_putchar_fd(' ', 1);
-			printed_spaces++;
-		}
+		if (flags[2] > addr_len + 2 - null_precision_pointer)
+			while (flags[2]-- > addr_len + 2 - null_precision_pointer)
+			{
+				ft_putchar_fd(' ', 1);
+				printed_spaces++;
+			}
 		write(1, "0x", 2);
-		print_ultoh((unsigned long)arg_pointer, 's');
+		if (!null_precision_pointer)
+			print_ultoh((unsigned long)arg_pointer, 's');
 	}
 	return (addr_len + printed_spaces + 2);
 }
@@ -634,7 +642,7 @@ static int	print_conversion_uint(unsigned int n, int *flags)
 static int	print_conversion_hexa(unsigned int n, int *flags, char check_casing)
 {
 	char	*converted_str;
-	char	conv_str_len;
+	int		conv_str_len;
 	int		precision;
 	int		printed_bytes;
 
@@ -734,28 +742,22 @@ static int	print_conversion_perc(int *flags)
 		precision = flags[3];
 	if (precision > 1) // pad precision - conv_str_len 0s
 	{
-		if (flags[2] > precision) // space padded by flags[2] - precision
+		printed_bytes = flags[2];
+		if (flags[0]) // left justified
 		{
-			printed_bytes = flags[2];
-			if (flags[0]) // left justified
-			{
-				ft_putchar_fd('%', 1);
-				while (--flags[2])
-					ft_putchar_fd(' ', 1);
-			}
-			else // right justified
-			{
-				while (--flags[2])
-					ft_putchar_fd(' ', 1);
-				ft_putchar_fd('%', 1);
-			}
-		}
-		else // not space padded, left justified by default
-		{
-			printed_bytes = precision;
 			ft_putchar_fd('%', 1);
-			while (--precision)
-				ft_putchar_fd('0', 1);
+			while (--flags[2])
+				ft_putchar_fd(' ', 1);
+		}
+		else // right justified
+		{
+			if (flags[1])
+				while (--flags[2])
+					ft_putchar_fd('0', 1);
+			else
+				while (--flags[2])
+					ft_putchar_fd(' ', 1);
+			ft_putchar_fd('%', 1);
 		}
 	}
 	else // precision less than or equal to str_len
